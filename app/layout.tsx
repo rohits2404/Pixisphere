2941740b1368471a4db1e872af75165bc640a49a
Rohit Sharma <rohits8731@gmail.com>
2025-06-14 12:05:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Pixisphere",
-    description: "Find The Best Photographers Near You.",
+    title: 'Pixisphere',
+    description: 'Find the best photographers near you',
     icons: {
         icon: "/icons/logo.png"
     }
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return(
+    return (
         <html lang="en">
             <body className="min-h-screen bg-gray-50 text-gray-900">
                 {children}
             </body>
         </html>
-    )
+    );
 }
